915d5b161bfd5387d236304baa732bd068aa37a4
plapointe6 <patrick.lapointe@hotmail.ca>
2019-04-01 18:49:16 -0400

    Change the default value of MAX_MQTT_PAYLOAD_SIZE the the value of PubSubClient
    Related to issue #1

--- src/EspMQTTClient.h
+++ src/EspMQTTClient.h
@@ -9,8 +9,11 @@
 
 #define MAX_TOPIC_SUBSCRIPTION_LIST_SIZE 10
 #define MAX_DELAYED_EXECUTION_LIST_SIZE 10
-#define MAX_MQTT_PAYLOAD_SIZE 256 // Maximum payload size, must correspond to MQTT_MAX_PACKET_SIZE of PubSubClient.h
 #define CONNECTION_RETRY_DELAY 10 * 1000
+
+// MQTT_MAX_PACKET_SIZE is defined in PubSubClient.h (default at 128 bytes)
+// If you want to augment the 128 byte maximum, you must change MQTT_MAX_PACKET_SIZE from PubSubClient.h
+#define MAX_MQTT_PAYLOAD_SIZE MQTT_MAX_PACKET_SIZE 
 
 typedef void(*ConnectionEstablishedCallback) ();
 typedef void(*MessageReceivedCallback) (const String &message);
